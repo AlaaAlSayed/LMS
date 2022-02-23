65a258a77330b30543a3ed3d0225e41f38b22c52
Fatma-Gamal <gamalfatma351@gmail.com>
2022-02-23 17:55:51 +0200

    finish chat

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_online')->default(0);
-            $table->string('last_activity')->nullable;
+            // $table->string('last_activity')->nullable;
+             $table->string('last_activity')->default(0);
 
             $table->rememberToken();
             $table->timestamps();
