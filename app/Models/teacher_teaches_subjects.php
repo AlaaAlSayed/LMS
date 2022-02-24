c451275461d14e57fc18cc419cfae701160503a9
AlaaAlSayed <alaa.elfaioumy@gmail>
2022-02-25 00:25:16 +0200

    teacher assign function

--- app/Models/teacher_teaches_subjects.php
+++ app/Models/teacher_teaches_subjects.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class teacher_teaches_subjects extends Model
 {
     use HasFactory;
+    public $timestamps = false;
     protected $fillable = [
         'teacherId',
         'subjectId',
