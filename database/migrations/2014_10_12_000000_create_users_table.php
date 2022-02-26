842d947aa92e86704d3fd82b2a46fe44f0d14149
AlaaAlSayed <alaa.elfaioumy@gmail>
2022-02-26 17:26:14 +0200

    database modification

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
 
             $table->string('name');
             $table->string('username')->unique();
-            $table->string('email')->unique();
+            // $table->string('email')->unique();
 
             $table->string('password');
             
@@ -32,7 +32,7 @@ return new class extends Migration
 
             $table->boolean('is_online')->default(0);
             $table->string('last_activity')->default(0);
-            $table->timestamp('email_verified_at')->nullable();
+            // $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
 
             $table->timestamps();
