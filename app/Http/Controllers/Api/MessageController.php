d7c913c99c1d1ce7804b5e10bba4d6cddbf242c2
AlaaAlSayed <alaa.elfaioumy@gmail>
2022-02-28 04:06:57 +0200

    message controller edit

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -3,9 +3,49 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    // 'message',
+    // 'user_id',
+    // 'receiver_id',
+    // 'is_seen'
+
+    public function index()
+    {
+        $allMessages = Message::all();
+        return  $allMessages->all();
+    }
+
+    public function show($messageId)
+    {
+        $message = Message::find($messageId);
+        return ($message);
+    }
+
+    public function showIcon($receiver_id)
+    {
+        $message = Message::find($receiver_id);
+        return ($message);
+    }
+
+    public function store() //StoreSubjectRequest $request)
+    {
+        $data = request()->all();
+
+        $message = Message::create([
+            // 'message',
+            // 'user_id',
+            // 'receiver_id',
+            // 'is_seen'
+            'message' => $data['message'],
+            'user_id' => $data['user_id'],
+            'receiver_id' => $data['receiver_id'],
+            'is_seen' => $data['is_seen'],
+        ]);
+
+        return ($message);
+    }
 }
