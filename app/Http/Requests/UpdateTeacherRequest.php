9e367dc9944a4f6c95e4ab1df43d9389951af525
AlaaAlSayed <alaa.elfaioumy@gmail>
2022-03-11 17:55:14 +0200

    student and teacher validation

--- app/Http/Requests/UpdateTeacherRequest.php
+++ app/Http/Requests/UpdateTeacherRequest.php
@@ -13,7 +13,7 @@ class UpdateTeacherRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,34 @@ class UpdateTeacherRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => ['required', 'min:3', 'max:30'],
+            'username' => ['required', 'min:5', 'max:15','unique:users'],
+           
+            'password' => ['required',  'min:8', 'max:20'],
+            'government' => ['required','min:4', 'max:10'],
+            'city' => ['required','min:4', 'max:10'],
+            'street' => ['required' ,'min:4', 'max:10'],
+        
+            'picture_path' => 'image|mimes:jpeg,pmb,png,jpg|max:88453',
+
+            'phone' => ['required',  'min:11', 'max:11','regex:/(01)[0-9]{9}/'],
+            'email' => ['required', 'min:10', 'max:30','email','unique:teachers'],
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'name.required' => 'A name is required',
+            'username.required' => 'username is required',
+            'password.required' => 'A password is required',
+            'government.required' => 'A government is required',
+            'city.required' => 'A city is required',
+            'street.required' => 'A street is required',
+
+            'phone.required' => 'A phone is required',
+            'email.required' => 'A email is required',
+
+            
         ];
     }
 }
