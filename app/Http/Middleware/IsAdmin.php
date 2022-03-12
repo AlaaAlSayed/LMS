a4afb4ce9d3527b3795af095bcfe08f85086b166
AlaaAlSayed <alaa.elfaioumy@gmail>
2022-03-12 15:42:19 +0200

    student middleware

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -29,16 +29,6 @@ class IsAdmin
             return $next($request); 
         } 
        
-        //  elseif (auth()->user()->roleId == 2) {
-
-        //     return redirect()->route('api.teachers.home', auth()->user()->id);
-
-        // } 
-        // elseif (auth()->user()->roleId == 3) {
-            
-        //     return redirect()->route('api.students.home', auth()->user()->id);
-        // }
-
         // //if not admin redirection
         return redirect()->route('home');
     }
